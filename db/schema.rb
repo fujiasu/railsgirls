e5cdad241cd9eeaa3ee8d38dd79871ab43a02396
fujiasu <fujiasu@gmail.com>
2018-05-19 16:17:23 +0900

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_064434) do
+ActiveRecord::Schema.define(version: 2018_05_19_071645) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
